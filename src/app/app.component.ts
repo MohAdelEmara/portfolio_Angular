73e8ba816fbcd5932ac4e4e666c7e56c4c8dfe2f
mohamed Emara <falacommohamed@gmail.com>
2024-03-31 00:05:06 -0300

    :sparkles: creating header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MainComponent } from './main/main.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [
+      RouterOutlet,
+      HeaderComponent,
+      FooterComponent,
+      MainComponent
+    
+    ]
 })
 export class AppComponent {
   title = 'my-portfolio';
